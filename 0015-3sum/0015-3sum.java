2925d1960abff0937e96b9244bcaed4a859794ed
Alok Goyal <alokgoyal634@gmail.com>
2026-07-29 00:27:09 +0530

    Time: 32 ms (78.61%), Space: 59 MB (86.06%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,5 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // Arrays.sort(nums);
-        // List<List<Integer>> List = new ArrayList<>();
-        // int target = 0;
-        // for(int i = 0; i<nums.length - 2; i++){
-        //     if(i>0 && nums[i] == nums[i - 1]){
-        //         continue;
-        //     }
-        //     int j = i + 1; int k = nums.length - 1;
-        //     while(j<k){
-        //         int sum = nums[i] + nums[j] + nums[k];
-        //         if(sum == target){
-        //             List.add(Arrays.asList(nums[i], nums[j], nums[k]));
-        //             while(j<k && nums[j] == nums[j+1]){
-        //                 j++;
-        //             }
-        //             while(j<k && nums[k] == nums[k-1]){
-        //                 k--;
-        //             }
-        //             j++;
-        //             k--;
-        //         }
-        //         else if(sum > target){
-        //             k--;
-        //         }
-        //         else if(sum < target){
-        //             j++;
-        //         } 
-        //     }
-        // }
-        // return List;
-
         Arrays.sort(nums); int n = nums.length;
         List<List<Integer>> result = new ArrayList<>();
         for(int i = 0; i<n-2; i++){
@@ -59,5 +28,8 @@ class Solution {
             }
         }
         return result;
+
+        // time complexity : O(nlogn) + O(n2) == O(n2);
+        // space complecity = O(n)
     }
 }
