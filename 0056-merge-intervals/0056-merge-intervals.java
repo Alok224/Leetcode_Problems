4e0dc4be054f16fb49e3b7342bf56d765bb72f2d
Alok Goyal <alokgoyal634@gmail.com>
2026-07-31 20:14:46 +0530

    Time: 8 ms (91.49%), Space: 49 MB (75.7%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        // List<int[]> list = new ArrayList<>();
+        // Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        // int[] newinterval = new int[2];
+        // newinterval = intervals[0];
+        // int i = 1; int n = intervals.length;
+        // while(i<n){
+        //     if(newinterval[1] >= intervals[i][0] && newinterval[0] <= intervals[i][1]){
+        //         newinterval[0] = Math.min(newinterval[0],intervals[i][0]);
+        //         newinterval[1] = Math.max(newinterval[1],intervals[i][1]);
+        //     }
+        //     else{
+        //         list.add(newinterval);
+        //         newinterval = intervals[i];
+        //     }
+        //     i++;
+        // }
+        // list.add(newinterval);
+        // return list.toArray(new int[list.size()][]);
+
+
+        List<int[]> list  = new ArrayList<>(); int n = intervals.length;
+        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
+        int[] newinterval = new int[2];
+        newinterval = intervals[0];
+        for(int i = 1; i<n; i++){
+            if(newinterval[0] <= intervals[i][1] && newinterval[1] >= intervals[i][0]){
+                newinterval[0] = Math.min(newinterval[0], intervals[i][0]);
+                newinterval[1] = Math.max(newinterval[1], intervals[i][1]);
+            }
+            else{
+                list.add(newinterval);
+                newinterval = intervals[i];
+            }
+        }
+        list.add(newinterval);
+        return list.toArray(new int[list.size()][]);
+    }
+}
