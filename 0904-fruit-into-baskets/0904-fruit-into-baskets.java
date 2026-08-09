971f8a1c20e25fd6060d99ab758ecdb011388a5c
Alok Goyal <alokgoyal634@gmail.com>
2026-08-09 16:24:21 +0530

    Time: 55 ms (42.67%), Space: 70.6 MB (29.29%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        // int i = 0;
+        // int j = 0;
+        // int k = 2;
+        // int max = 0;
+        // // create a map
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // while(j<fruits.length){
+        //     // always add the fruit
+        //     map.put(fruits[j],map.getOrDefault(fruits[j],0) + 1);
+
+        //     if(k<map.size()){
+        //         while(k<map.size()){
+        //             map.put(fruits[i],map.get(fruits[i]) -1);
+        //             if(map.get(fruits[i]) == 0){
+        //                 // remove it from the map
+        //                 map.remove(fruits[i]);
+        //             }
+        //             i++;
+        //         }
+        //     }
+
+        //     if(k == map.size()){
+        //         // find the max element
+        //         max = Math.max(max,j-i+1);
+               
+        //     }
+        //     j++;
+        // }
+        // if(k>map.size()){
+        //     max = j-i;
+        // }
+        // return max;
+
+        int i = 0; int j = 0; int k = 2; int maxlen = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while(j<fruits.length){
+            map.put(fruits[j],map.getOrDefault(fruits[j],0) + 1);
+
+            while(map.size() > k){
+                map.put(fruits[i],map.get(fruits[i]) - 1);
+                if(map.get(fruits[i]) == 0){
+                    map.remove(fruits[i]);
+                }
+                i++;
+            }
+            maxlen = Math.max(maxlen, j-i+1);
+            j++;
+        }
+        return maxlen;
+    }
+}
