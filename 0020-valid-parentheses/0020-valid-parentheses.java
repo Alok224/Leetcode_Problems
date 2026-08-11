d40d8063b2bbe51e6ac3faf0ea0a9dcbaf3b808c
Alok Goyal <alokgoyal634@gmail.com>
2026-08-12 00:54:55 +0530

    Time: 4 ms (36.72%), Space: 43.9 MB (5.73%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,53 @@
+class Solution {
+    // public static boolean helper(String s){
+    //     // create a stack data structure
+    //     Stack<Character> stack = new Stack<>();
+
+    //     for(int i = 0; i<s.length(); i++){
+    //         if(s.charAt(i) == '('){
+    //             stack.push(')');
+    //         }
+    //         else if(s.charAt(i) == '{'){
+    //             stack.push('}');
+    //         }
+    //         else if(s.charAt(i) == '['){
+    //             stack.push(']');
+    //         }
+
+    //         else if(stack.isEmpty() || stack.pop() != s.charAt(i)){
+    //             return false;
+    //         }
+            
+    //     }
+    //     return stack.isEmpty();
+    // }
+    public boolean isValid(String s) {
+        // return helper(s);
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                st.push('(');
+            }
+            else if(s.charAt(i) == '['){
+                st.push('[');
+            }
+            else if(s.charAt(i) == '{'){
+                st.push('{');
+            }
+
+            else if(st.isEmpty() == false && s.charAt(i) == ')' && st.peek() == '('){
+                st.pop();
+            }
+            else if(st.isEmpty() == false && s.charAt(i) == '}' && st.peek() == '{'){
+                st.pop();
+            }
+            else if(st.isEmpty() == false && s.charAt(i) == ']' && st.peek() == '['){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return st.isEmpty() == true? true : false;
+    }
+}
