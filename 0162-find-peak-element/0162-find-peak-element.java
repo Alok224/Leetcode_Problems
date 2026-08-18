f306d703cffe3996622e55d308d8e3c460f9bd00
Alok Goyal <alokgoyal634@gmail.com>
2026-08-18 17:44:36 +0530

    Time: 0 ms (100%), Space: 44.3 MB (38.6%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -11,24 +11,12 @@ class Solution {
         // }
         // return index;
 
-
-
-
-        int low = 0;
-        int high = nums.length - 1;
-
-        while (low < high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] > nums[mid + 1]) {
-                high = mid;
-            }
-            else {
-                low = mid + 1;
+        int n = nums.length; int index = 0;
+        for(int i = 0; i<n; i++){
+            if(i == 0 || nums[i-1] < nums[i]){
+                index = i;
             }
         }
-
-        return low;
+        return index;
     }
 }
