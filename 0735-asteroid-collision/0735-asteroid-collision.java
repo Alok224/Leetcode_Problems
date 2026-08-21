bd405602e610d58375a63374e382c93a5d3083bb
Alok Goyal <alokgoyal634@gmail.com>
2026-08-21 11:14:45 +0530

    Time: 2 ms (97.77%), Space: 46.7 MB (83.09%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,23 +1,51 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        // int n = asteroids.length;
+        // List<Integer> list = new ArrayList<>();
+        // for(int i = 0; i<n; i++){
+        //     if(asteroids[i] > 0){
+        //         list.add(asteroids[i]);
+        //     }
+        //     else{
+        //         while(list.isEmpty() == false && list.get(list.size() - 1) > 0 && list.get(list.size() - 1) < Math.abs(asteroids[i])){
+        //             list.remove(list.size() - 1);
+        //         }
+        //         if(list.isEmpty() == false && list.get(list.size() - 1) == Math.abs(asteroids[i])){
+        //             list.remove(list.size() - 1);
+        //         }
+        //         else if(list.isEmpty() == true || list.get(list.size() - 1) < 0){
+        //             list.add(asteroids[i]);
+        //         }
+        //     }
+        // }
+        // int[] result = new int[list.size()];
+        // for(int i = 0; i<list.size(); i++){
+        //     result[i] = list.get(i);
+        // }
+        // return result;
+
         int n = asteroids.length;
         List<Integer> list = new ArrayList<>();
+
         for(int i = 0; i<n; i++){
             if(asteroids[i] > 0){
                 list.add(asteroids[i]);
             }
             else{
-                while(list.isEmpty() == false && list.get(list.size() - 1) > 0 && list.get(list.size() - 1) < Math.abs(asteroids[i])){
+                while(list.size() != 0 && list.get(list.size() - 1) > 0 && list.get(list.size() - 1) < Math.abs(asteroids[i])){
                     list.remove(list.size() - 1);
                 }
-                if(list.isEmpty() == false && list.get(list.size() - 1) == Math.abs(asteroids[i])){
+
+                if(list.size() != 0 && list.get(list.size() - 1) > 0 && list.get(list.size() - 1) == Math.abs(asteroids[i])){
                     list.remove(list.size() - 1);
                 }
-                else if(list.isEmpty() == true || list.get(list.size() - 1) < 0){
+
+                else if(list.size() == 0 || list.get(list.size() - 1) < 0){
                     list.add(asteroids[i]);
                 }
             }
         }
+
         int[] result = new int[list.size()];
         for(int i = 0; i<list.size(); i++){
             result[i] = list.get(i);
