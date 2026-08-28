42270ddaac97a43a85b16b62e63962f0b77cc01a
Alok Goyal <alokgoyal634@gmail.com>
2026-08-28 18:49:53 +0530

    Time: 15 ms (60.21%), Space: 51.8 MB (6.43%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int left = 0; int right = 0; int n = g.length; int m = s.length;
+        Arrays.sort(g); Arrays.sort(s);
+        while(right < m && left < n){
+            if(g[left] <= s[right]){
+                left = left + 1;
+            }
+            right = right + 1;
+        }
+        return left;
+    }
+}
